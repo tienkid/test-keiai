52cb1949243b0640b732389ac5774e61ffa031d7
uri <uri.vu@vmodev.com>
2022-12-12 09:59:50 +0700

    fix(mobile): fix point screen

--- src/app/features/un-authentication/login/index.tsx
+++ src/app/features/un-authentication/login/index.tsx
@@ -62,7 +62,7 @@ const LoginComponent = () => {
         const diff = moment.duration(moment(expired).diff(moment(today)));
         const days = diff.days();
         console.log(days, 'days');
-        if (days <= 0) {
+        if (days > 0) {
           navigate(APP_SCREEN.REGISTER, { type: 'reLogin' });
         } else {
           dispatch(
